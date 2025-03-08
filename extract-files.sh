5b894b38b8677fa49fdcfdf5564941ac641dea8c
Rinigus <rinigus.git@gmail.com>
2025-03-08 10:29:32 +0200

    pdx223: use VNDK32 for camera
    Fix abort message: incStrongRequireStrong() called on 0xb40000732a7c5920 which isn't already owned

--- extract-files.sh
+++ extract-files.sh
@@ -8,6 +8,17 @@
 
 function blob_fixup() {
     case "${1}" in
+        vendor/lib64/vendor.somc.camera* | vendor/bin/hw/vendor.somc.hardware.camera.*)
+            [ "$2" = "" ] && return 0
+            grep -q "libutils-v32.so" "${2}" || "${PATCHELF}" --replace-needed "libutils.so" "libutils-v32.so" "${2}"
+            grep -q "libhidlbase-v32.so" "${2}" || "${PATCHELF}" --replace-needed "libhidlbase.so" "libhidlbase-v32.so" "${2}"
+            grep -q "libbinder-v32.so" "${2}" && return 0
+            if ! "${PATCHELF}" --print-needed "${2}" | grep "libbinder.so" > /dev/null; then
+                "${PATCHELF}" --add-needed "libbinder-v32.so" "${2}"
+            else
+                "${PATCHELF}" --replace-needed "libbinder.so" "libbinder-v32.so" "${2}"
+            fi
+            ;;
         vendor/lib64/libcamximageformatutils.so)
             [ "$2" = "" ] && return 0
             ${PATCHELF} --replace-needed "vendor.qti.hardware.display.config-V2-ndk_platform.so" "vendor.qti.hardware.display.config-V2-ndk.so" "${2}"
