6cd37727f699202bd1637068e443826fd668fbb3
Rinigus <rinigus.git@gmail.com>
2025-05-12 01:17:02 +0700

    pdx224: Update kernel config path
    Change-Id: I873fa7a83a3471f29ace7f9bd5265df2b6b0912e

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -9,7 +9,7 @@ include device/sony/sm8450-common/BoardConfigCommon.mk
 DEVICE_PATH := device/sony/pdx224
 
 # Kernel
-TARGET_KERNEL_CONFIG += pdx224_defconfig
+TARGET_KERNEL_CONFIG += vendor/sony/pdx224.config
 
 # Properties
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
